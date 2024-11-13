fa703e2afe4724cdd1df02b7f71e9b5de02f459c
Hyowon Ahn <hyowon.ahn@student.mancehster.ac.uk>
2024-11-13 23:11:37 +0000

    aliens will be reset once they are all attacked

--- game_solution.py
+++ game_solution.py
@@ -1,14 +1,12 @@
 # Space Invaders 
 
-from tkinter import Tk, Canvas, PhotoImage, Label
+from tkinter import Tk, Canvas, PhotoImage, Label, simpledialog, Toplevel, ttk, Radiobutton
 import random
 from PIL import ImageTk, Image
 
 class constants:
-    width = 750
-    height = 950
-    life = 3
-    alinePerLine = 8
+    WIDTH = 750
+    HEIGHT = 950
 
 class game_objects:
     def __init__(self, canvas, obj):
@@ -50,9 +48,9 @@ class Cannon(game_objects):
         item = canvas.create_image(x, y, image=ix)
         """
         item = canvas.create_rectangle(355, 850, 395, 890, fill="yellow")
-        self.lives = constants.life
+        self.lives = 3
         self.lifeArray = []
-        for i in range(constants.life):
+        for i in range(3):
             self.lifeArray.append(canvas.create_rectangle(600+40*i, 25, 625+40*i, 50, fill="red"))
         super(Cannon, self).__init__(canvas, item)
     
@@ -141,8 +139,8 @@ class gameBoard(Canvas):
             self,
             window,
             bg="#000000",
-            width=constants.width,
-            height=constants.height
+            width=constants.WIDTH,
+            height=constants.HEIGHT
         )
         self.pack()
         self.score = 0
@@ -158,16 +156,24 @@ class gameBoard(Canvas):
         
         self.hitEdge = 0
         self.aliens = []
-        for i in range(5): 
+        self.alienRow = 2
+        self.alienCol = 8
+        self.alienCnt = self.alienRow * self.alienCol
+        for row in range(self.alienRow): 
             line = []
-            for j in range(8):
-                line.append(Alien(self, 145+60*j, 75+60*i, 185+60*j, 115+60*i))
+            for col in range(self.alienCol):
+                line.append(Alien(self, 145+60*col, 75+60*row, 185+60*col, 115+60*row))
             self.aliens.append(line)
         self.projectiles = []
-        self.gameLoop()
+        self.signIn = True
+        if self.signIn:
+            self.gameLoop()
 
     def gameLoop(self):
         """Main game loop to handle movements, projectile firing, and collision checks."""
+        if self.alienCnt == 0:
+            self.resetAliens()
+        
         # Move aliens
         if self.hitEdge % 2 == 0:
             self.moveAlienR()
@@ -191,25 +197,36 @@ class gameBoard(Canvas):
         else:
             # Game over, end the loop, and display a message
             self.create_text(
-                constants.width // 2,
-                constants.height // 2,
+                constants.WIDTH // 2,
+                constants.HEIGHT // 2,
                 text="Game Over",
                 fill="red",
                 font="Helvetica 30 bold"
             )
+    
+    def resetAliens(self):
+        self.aliens = []  # Clear existing aliens
+        self.alienCnt = 40  # Reset alien count
+
+        for row in range(self.alienRow): 
+            line = []
+            for col in range(self.alienCol):
+                # Recreate each alien in its original position
+                line.append(Alien(self, 145 + 60 * col, 75 + 60 * row, 185 + 60 * col, 115 + 60 * row))
+            self.aliens.append(line)
     
     def moveCannon(self):
         """Move the cannon smoothly based on current key flags."""
         pos = self.cannon.get_position()
         if self.cannonMoveLeft and pos[0] > 5:
             self.cannon.moveLeft()
-        elif self.cannonMoveRight and pos[2] < constants.width - 5:
+        elif self.cannonMoveRight and pos[2] < constants.WIDTH - 5:
             self.cannon.moveRight()
     
     def fire(self):
         """Fire razor when user press space and check collisions"""
         self.razor.state("normal")
-        self.razor.move_to(0, -5)
+        self.razor.move_to(0, -15)
         posRazor = self.razor.get_position()
         """Check if razor is out of bound(top of screen)"""
         if posRazor[1] <= 0:
@@ -226,55 +243,87 @@ class gameBoard(Canvas):
                     self.attack = False
                     
         """Check for collision with aliens"""
-        for i in range(5):
-            for j in range(8):
-                state = self.aliens[i][j].getState()
+        for row in range(self.alienRow):
+            for col in range(self.alienCol):
+                state = self.aliens[row][col].getState()
                 if state != "hidden": 
-                    posAlien = self.aliens[i][j].get_position()
+                    posAlien = self.aliens[row][col].get_position()
                     if posAlien[0] < posRazor[2] < posAlien[2] and posAlien[1] < posRazor[1] < posAlien[3]:
                         self.razor.delete()
-                        self.aliens[i][j].state("hidden")
+                        self.aliens[row][col].state("hidden")
+                        self.alienCnt -= 1
                         self.score += 100
                         self.itemconfigure(self.scoreText, text=("Score: " + str(self.score)))
                         self.attack = False
         
     def moveAlienR(self):
-        pos = self.aliens[0][7].get_position()
-        for i in range(5):
-            for j in range(8):
-                state = self.aliens[i][j].getState()
+        rightCol = self.getRightmostActiveColumn()  # get the current rightmost active column
+        if rightCol == -1:
+            return  # no aliens left, exit the function
+        
+        pos = None
+        for row in range(self.alienRow):
+            pos = self.aliens[row][rightCol].get_position()
+            if pos:  # get the position of the first visible alien in the column
+                break
+        for row in range(self.alienRow):
+            for col in range(self.alienCol):
+                state = self.aliens[row][col].getState()
                 if state != "hidden": 
-                    self.aliens[i][j].move_to(1, 0)
-        if pos[2] > constants.width - 15:
+                    self.aliens[row][col].move_to(1, 0)
+        if pos[2] > constants.WIDTH - 15:
             self.hitEdge += 1
             
     def moveAlienL(self):
-        pos = self.aliens[0][0].get_position()
-        for i in range(5):
-            for j in range(8):
-                state = self.aliens[i][j].getState()
+        leftCol = self.getLeftmostActiveColumn()  # get the current leftmost active column
+        if leftCol == -1:
+            return  # no aliens left, exit the function
+        
+        pos = None
+        for row in range(self.alienRow):
+            pos = self.aliens[row][leftCol].get_position()
+            if pos:  # get the position of the first visible alien in the column
+                break
+            
+        for row in range(self.alienRow):
+            for col in range(self.alienCol):
+                state = self.aliens[row][col].getState()
                 if state != "hidden": 
-                    self.aliens[i][j].move_to(-1, 0)
+                    self.aliens[row][col].move_to(-1, 0)
         if pos[0] < 15:
             self.hitEdge += 1
             if self.hitEdge == 4:
                 self.moveAlienDown()
                 self.hitEdge = 0
     
+    def getLeftmostActiveColumn(self):
+        for col in range(self.alienCol):  # iterate from the leftmost to rightmost columns
+            for row in range(self.alienRow):  # check each row in the column
+                if self.aliens[row][col].getState() != "hidden":
+                    return col  # return the first column with a visible alien
+        return -1  # if all aliens are hidden
+    
+    def getRightmostActiveColumn(self):
+        for col in range(7, -1, -1):  # iterate from the rightmost to leftmost columns
+            for row in range(self.alienRow):  # check each row in the column
+                if self.aliens[row][col].getState() != "hidden":
+                    return col  # return the first column with a visible alien
+        return -1  # if all aliens are hidden
+    
     def moveAlienDown(self):
-        for i in range(5):
-            for j in range(8):
-                state = self.aliens[i][j].getState()
+        for row in range(self.alienRow):
+            for col in range(self.alienCol):
+                state = self.aliens[row][col].getState()
                 if state != "hidden": 
-                    self.aliens[i][j].move_to(0, 60)
+                    self.aliens[row][col].move_to(0, 60)
     
     def fireProjectile(self):
         """Fire projectiles from 3 random aliens and check collisions"""
         # Ensure we have a maximum of 3 active projectiles on the screen
         if len(self.projectiles) < 3:
             # Randomly select an alien to fire a projectile
-            x = random.randint(0, 4)
-            y = random.randint(0, 7)
+            x = random.randint(0, self.alienRow - 1)
+            y = random.randint(0, self.alienCol - 1)
             
             # Check if the selected alien is visible (not hidden)
             if self.aliens[x][y].getState() != 'hidden':
@@ -288,7 +337,7 @@ class gameBoard(Canvas):
             posProjectile = p.get_position()
             
             # Check if projectile is out of bounds (bottom of screen)
-            if posProjectile[1] >= constants.height:
+            if posProjectile[1] >= constants.HEIGHT:
                 p.delete()
                 self.projectiles.remove(p)
                 continue  # Skip further checks for this projectile
@@ -318,7 +367,7 @@ def keyPressed(event):
     posCannon = board.cannon.get_position()
     if event.keysym == "Left" and posCannon[0] > 5:
         board.cannonMoveLeft = True
-    elif event.keysym == "Right" and posCannon[2] < constants.width - 5:
+    elif event.keysym == "Right" and posCannon[2] < constants.WIDTH - 5:
         board.cannonMoveRight = True
     elif event.keysym == "space" and board.attack == False:
         board.attack = True
@@ -332,16 +381,28 @@ def keyReleased(event):
     elif event.keysym == "Right":
         board.cannonMoveRight = False
 
+def open_popup():
+        initWin = Tk()
+        initWin.geometry("750x250")
+        initWin.title("Sign in")
+        r1=Radiobutton(initWin, text="Use arrows keys to move and spacekey to fire", value=1)
+        r2=Radiobutton(initWin, text="Use A and D to move and spacekey to fire", value=2)
+        r3=Radiobutton(initWin, text="Use A and D to move and W to fire", value=3)
+
+        r1.pack()
+        r2.pack()
+        r3.pack()
+
 
 window = Tk()
 window.title("Space Invaders")
-
+open_popup()
 ws = window.winfo_screenwidth()  # computers screen size
 hs = window.winfo_screenheight()
-x = (ws / 2) - (constants.width / 2)  # calculate center
-y = (hs / 2) - (constants.height / 2)
+x = (ws / 2) - (constants.WIDTH / 2)  # calculate center
+y = (hs / 2) - (constants.HEIGHT / 2)
 window.geometry(
-    "%dx%d+%d+%d" % (constants.width, constants.height, x, y)
+    "%dx%d+%d+%d" % (constants.WIDTH, constants.HEIGHT, x, y)
 )  # show the window at the middle of the screen
 window.resizable(False, False)
 window.tk.call("tk", "scaling", 4.0)
